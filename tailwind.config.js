5991c24ad127110c9220f26492c7078a9f9f0290
Caio Cezar <114605074+Skarzyll@users.noreply.github.com>
2024-07-06 16:58:15 -0300

    update with tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'body-color': 'hsl(30, 38%, 92%)',
+        'color-p': 'hsl(158, 36%, 37%);',
+        
+      },
+
+      fontFamily: {
+        'font-h1': ['Fraunces', 'serif']
+      },
+
+      backgroundImage: {
+        'imagemobile': "url('/src/images/image-product-mobile.jpg')",
+
+        'imagedesktop': "url('/src/images/image-product-desktop.jpg')",
+      },
+      height: {
+        'sect': '450px'
+      }
+    },
+  },
+  plugins: [],
+}
+
